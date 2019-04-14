6e150a28caa268c97c2aa2ded68c12d693fa25a8
grigiriy <gregorymalyshev@MBP-Gregory.Dlink>
2019-04-14 16:13:48 +0300

    set OK datepicker

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Row, Col, Dropdown } from 'react-bootstrap';
-import DayPicker from 'react-daypicker';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
 
-import 'react-daypicker/lib/DayPicker.css';
+import 'react-day-picker/lib/style.css';
 import  './book.css';
 
 import img from '../../assets/img/second_block.png';
@@ -16,6 +16,10 @@ class TextBlock extends Component {
         time: '19:00',
         calend: false,
     };
+
+     handleDayChange = async (day) => {
+        this.setState({ day: day });
+     }
 
     handleSticky = event => {
         let startSticky = document.getElementById('book').offsetTop;
@@ -34,27 +38,6 @@ class TextBlock extends Component {
         }
     }
 
-    toggleCalend() {
-        // let toggledCalend = !this.state.calend;
-        this.setState({ calend: true });
-        if (!this.state.calend) {
-          document.addEventListener('click', this.handleOutsideClick, false);
-        } else {
-          document.removeEventListener('click', this.handleOutsideClick, false);
-        }
-        this.setState(prevState => ({
-           calend: !prevState.calend,
-        }));
-      }
-      toggleCalendOutside(e) {
-        // ignore clicks on the component itself
-        if (this.node.contains(e.target)) {
-          return;
-        }
-
-        this.toggleCalend();
-    }
-
     // Init
 	componentDidMount() {
 		window.addEventListener('scroll', this.handleSticky);
@@ -68,12 +51,12 @@ class TextBlock extends Component {
         const sticky = this.state.sticky ? 'b-sticky' : '';
         let persons = this.state.persons + ' persons';
         return <>
-        <Row id="book" className={"screen book-screen " + sticky}>
-            <Col sm={12} className="mobile mobile_book_pic w-100">
-                <img src={img} alt="" style={{maxWidth:'100%'}} />
+        <Row id="book" className={ "screen book-screen " + sticky } >
+            <Col sm={ 12 } className="mobile mobile_book_pic w-100">
+                <img src={ img } alt="" style={{ maxWidth:'100%' }} />
             </Col>
-            <Col sml={12} className="mx-auto d-flex z-10">
-                <form  style={{minHeight:'650px'}} id="form">
+            <Col sml={ 12 } className="mx-auto d-flex z-10">
+                <form  style={{ minHeight:'650px' }} id="form">
                     <span className="_form" id="huy" data-time="12">
                         <h2 className="text-center">Book a table</h2>
                         <p className="text-center">Three days a week, from Thursday to Saturday, we serve set-dinner on pre-reservation.</p>
@@ -83,17 +66,16 @@ class TextBlock extends Component {
                                 <div className="line-inputs row">
                                     <div className="col-md-7 col-sm-12 schedule-inputs">
                                         <div className="d-flex schedule">
-                                            <span className="schedule--date" onClick={ () => this.toggleCalend() }>
-                                                <input className="calend_input" type="text" name="date" id="date" value={ this.state.day } />
-                                                { this.state.calend && (<DayPicker onDayClick={(day) => this.setState({ day })} />) }
+                                            <span className="schedule--date" >
+                                                <DayPickerInput onDayChange={ this.handleDayChange }/>
                                             </span>
                                             <Dropdown className="schedule--time">
-                                                <Dropdown.Toggle id="schedule" type="button" data-toggle="dropdown">{this.state.time}</Dropdown.Toggle>
+                                                <Dropdown.Toggle id="schedule" type="button" data-toggle="dropdown">{ this.state.time }</Dropdown.Toggle>
                                                 <Dropdown.Menu aria-labelledby="dropdownMenuButton">
                                                     <div className="dropdown-wrap">
-                                                        <Dropdown.Item onClick={ () => this.setState({time: '19:00'}) }>19:00</Dropdown.Item>
-                                                        <Dropdown.Item onClick={ () => this.setState({time: '20:00'}) }>20:00</Dropdown.Item>
-                                                        <Dropdown.Item onClick={ () => this.setState({time: '21:00'}) }>21:00</Dropdown.Item>
+                                                        <Dropdown.Item onClick={ () => this.setState({ time: '19:00' }) }>19:00</Dropdown.Item>
+                                                        <Dropdown.Item onClick={ () => this.setState({ time: '20:00' }) }>20:00</Dropdown.Item>
+                                                        <Dropdown.Item onClick={ () => this.setState({ time: '21:00' }) }>21:00</Dropdown.Item>
                                                     </div>
                                                 </Dropdown.Menu>
                                             </Dropdown>
@@ -110,7 +92,7 @@ class TextBlock extends Component {
                             <button id="confirm" >book now</button>
                         </div>
                     </span>
-                    <div className="_success" style={{display:'none', textAlign:'center'}}>
+                    <div className="_success" style={{ display:'none', textAlign:'center' }}>
                         <svg id="successAnimation" className="animated" xmlns="http://www.w3.org/2000/svg" width="170" height="170" viewBox="0 0 70 70">
                           <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
                           <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" strokeWidth="2" strokeLinecap="round" fill="transparent"/>
