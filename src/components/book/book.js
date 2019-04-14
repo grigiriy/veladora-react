5e85f4970d33bd6f9f0c6afadce08366907057ae
grigiriy <gregorymalyshev@MBP-Gregory.Dlink>
2019-04-15 01:52:03 +0300

    success sent text animation

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Row, Col, Dropdown } from 'react-bootstrap';
+import MaskedInput from 'react-text-mask'
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import MomentLocaleUtils, { formatDate, parseDate } from 'react-day-picker/moment';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import 'moment/locale/it';
 
 import 'react-day-picker/lib/style.css';
@@ -17,7 +19,10 @@ class TextBlock extends Component {
         day: 'Today',
         time: '19:00',
         calend: false,
+        phone: '',
+        name: ''
     };
+
 
      handleDayClick = async (day) => {
         this.setState({ day: day });
@@ -58,7 +63,14 @@ class TextBlock extends Component {
             </Col>
             <Col sml={ 12 } className="mx-auto d-flex z-10">
                 <form  style={{ minHeight:'650px' }} id="form">
-                    <span className="_form" id="huy" data-time="12">
+                <ReactCSSTransitionGroup
+                    transitionName="book"
+                    transitionAppear = { false }
+                    transitionEnterTimeout = { 1000 }
+                    transitionLeave = { false }
+                >
+                { !this.state.sent && (
+                    <div className="_form" key={this.state.sent}>
                         <h2 className="text-center">Book a table</h2>
                         <p className="text-center">Three days a week, from Thursday to Saturday, we serve set-dinner on pre-reservation.</p>
                         <div className="d-flex text-block">
@@ -99,20 +111,27 @@ class TextBlock extends Component {
                                         <span id="plus" onClick={ () => this.addPerson() }>+</span>
                                     </div>
                                 </div>
-                                <input type="phone" placeholder="phone number" className="w-100" />
+                                <MaskedInput type="phone" placeholder="phone number" className="w-100" />
                             </div>
-                            <button id="confirm" >book now</button>
+                            <button id="confirm" onClick={ () => this.setState({sent: true}) }>book now</button>
                         </div>
-                    </span>
-                    <div className="_success" style={{ display:'none', textAlign:'center' }}>
-                        <svg id="successAnimation" className="animated" xmlns="http://www.w3.org/2000/svg" width="170" height="170" viewBox="0 0 70 70">
-                          <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
-                          <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" strokeWidth="2" strokeLinecap="round" fill="transparent"/>
-                          <polyline id="successAnimationCheck" stroke="#979797" strokeWidth="2" points="23 34 34 43 47 27" fill="transparent"/>
-                        </svg>
-                        <h2 className="text-center">Thank You</h2>
-                        <p className="text-center">Your table is now booked. We will get in touch to re-confirm your reservation.</p>
                     </div>
+                    ) }
+                    { this.state.sent && (
+                    <div className="_success" key={this.state.sent}>
+                    <svg id="successAnimation" className="animated" xmlns="http://www.w3.org/2000/svg" width="170" height="170" viewBox="0 0 70 70">
+                      <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
+                      <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" strokeWidth="2" strokeLinecap="round" fill="transparent"/>
+                      <polyline id="successAnimationCheck" stroke="#979797" strokeWidth="2" points="23 34 34 43 47 27" fill="transparent"/>
+                        </svg>
+                        <span className="successText">
+                            <h2 className="text-center">Thank You</h2>
+                            <p className="text-center">Your table is now booked. We will get in touch to re-confirm your reservation.</p>
+                        </span>
+                        <button id="confirm" onClick={ () => this.setState({sent: false}) }>book now</button>
+                    </div>
+                    ) }
+                    </ReactCSSTransitionGroup>
                 </form>
             </Col>
         </Row>
