0d55bc8896235c0198d5f9f3a59f1aee41c3587a
grigiriy <gregorymalyshev@MBP-Gregory.Dlink>
2019-04-15 06:32:48 +0300

    fix sticky on slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -5,18 +5,44 @@ import  './slider.css';
 class Slider extends Component {
 
     state = {
-        firstScreen: this.props
+        firstScreen: this.props.firstScreen,
+        sticky: false,
+        slider: this.props
     };
+
+    handleSticky = event => {
+        if (!this.state.firstScreen){
+            let startSticky = document.getElementById('menu').offsetTop;
+            let endSticky = document.getElementById('video').offsetTop;
+            (window.scrollY > startSticky && window.scrollY < endSticky) ? this.setState({sticky: true}) : this.setState({sticky: false});
+        }
+    }
+
+
+    componentDidMount() {
+        if (!this.state.firstScreen){
+    		window.addEventListener('scroll', this.handleSticky);
+    		this.handleSticky();
+        }
+	}
+	componentWillUnmount() {
+        if (!this.state.firstScreen){
+    		window.removeEventListener('scroll', this.handleSticky);
+    		this.handleSticky();
+        }
+	}
 
     render() {
 
+        const sticky = this.state.sticky ? 'sticky' : '';
         const slide = this.props.slider;
+        const counter = this.state.firstScreen ? '1' : '2';
 
-        return <Row className="">
+        return <Row className={sticky}>
             <Carousel
                 nextIcon={<span></span>}
                 prevIcon={<span></span>}
-                id="carousel_1"
+                id={"slider_" + counter}
                 className="slide col"
                 data-ride="carousel"
             >
