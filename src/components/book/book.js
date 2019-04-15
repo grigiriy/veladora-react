43e2152f137c8e3d34ca59321034798ffc8f87da
grigiriy <gregorymalyshev@MBP-Gregory.Dlink>
2019-04-15 13:25:18 +0300

    almost done with svg on bookSuccess

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -66,7 +66,7 @@ class TextBlock extends Component {
                 <ReactCSSTransitionGroup
                     transitionName="book"
                     transitionAppear = { false }
-                    transitionEnterTimeout = { 1000 }
+                    transitionEnterTimeout = { 700 }
                     transitionLeave = { false }
                 >
                 { !this.state.sent ? (
@@ -113,20 +113,20 @@ class TextBlock extends Component {
                                 </div>
                                 <MaskedInput type="phone" placeholder="phone number" className="w-100" />
                             </div>
+                        <button id="confirm" onClick={ () => this.setState({sent: true}) }>book now</button>
                         </div>
                     </div>
                     ) : (
                     <div className="_success" key={this.state.sent}>
-                    <svg id="successAnimation" className="animated" xmlns="http://www.w3.org/2000/svg" width="170" height="170" viewBox="0 0 70 70">
-                      <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
-                      <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" strokeWidth="2" strokeLinecap="round" fill="transparent"/>
-                      <polyline id="successAnimationCheck" stroke="#979797" strokeWidth="2" points="23 34 34 43 47 27" fill="transparent"/>
+                        <svg id="successAnimation" className="animated" xmlns="http://www.w3.org/2000/svg" width="170" height="170" viewBox="0 0 70 70">
+                          <path id="successAnimationResult" fill="#fff" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
+                          <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#000" strokeWidth="2" strokeLinecap="round" fill="transparent"/>
+                          <polyline id="successAnimationCheck" stroke="#000" strokeWidth="2" points="23 34 34 43 47 27" fill="transparent"/>
                         </svg>
                         <span className="successText">
                             <h2 className="text-center">Thank You</h2>
                             <p className="text-center">Your table is now booked. We will get in touch to re-confirm your reservation.</p>
                         </span>
-                        <button id="confirm" onClick={ () => this.setState({sent: false}) }>book now</button>
                     </div>
                     ) }
                     </ReactCSSTransitionGroup>
