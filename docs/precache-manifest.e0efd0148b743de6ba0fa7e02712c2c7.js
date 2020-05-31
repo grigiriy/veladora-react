8517f3a989265146d8b42c1a383e7a1087c514ff
Grigiriy <grigiriy.malyshev@gmail.com>
2020-05-31 15:00:55 +0300

    gh-pages

---
+++ docs/precache-manifest.e0efd0148b743de6ba0fa7e02712c2c7.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f165d1ff8ad081d6cf1d6be4d1caa8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "751e2c2b46a33baae9fc",
+    "url": "./static/css/2.c632958e.chunk.css"
+  },
+  {
+    "revision": "c7ed6f512df0b15cf210",
+    "url": "./static/css/main.fcbd99e5.chunk.css"
+  },
+  {
+    "revision": "751e2c2b46a33baae9fc",
+    "url": "./static/js/2.6d0895d4.chunk.js"
+  },
+  {
+    "revision": "88be2ffdd0187a26c1ead6f9673fbb5a",
+    "url": "./static/js/2.6d0895d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ed6f512df0b15cf210",
+    "url": "./static/js/main.8bf0bb7a.chunk.js"
+  },
+  {
+    "revision": "7a833fefead15dc83aa8",
+    "url": "./static/js/runtime-main.d6a7b5d6.js"
+  },
+  {
+    "revision": "8eee3f5a7b2288856c25a6609836ac58",
+    "url": "./static/media/1-video.8eee3f5a.png"
+  },
+  {
+    "revision": "99dfa262336304b646e9b2e330920ffa",
+    "url": "./static/media/2-video.99dfa262.png"
+  },
+  {
+    "revision": "59f21e370a3c40c63b02d66ebef629c5",
+    "url": "./static/media/3-video.59f21e37.png"
+  },
+  {
+    "revision": "b71bba9f85f548528e62d6639ce2d814",
+    "url": "./static/media/4-video.b71bba9f.png"
+  },
+  {
+    "revision": "fdf1ec96f7e461eaebf70db68c5e66d1",
+    "url": "./static/media/Berlingske-Serif-ExtraBold.fdf1ec96.woff"
+  },
+  {
+    "revision": "f2b915a52d7450d8f0b8abd50bc6796d",
+    "url": "./static/media/apercu-medium-pro.f2b915a5.woff"
+  },
+  {
+    "revision": "7c7991b0800ade5f42ff77b37a2344f9",
+    "url": "./static/media/apercu-regular-pro.7c7991b0.woff"
+  },
+  {
+    "revision": "f00a0aa500e55cf005d91b5a70458bb2",
+    "url": "./static/media/arrow_next.f00a0aa5.svg"
+  },
+  {
+    "revision": "cbd381fdf08ebdc34cfcd682ba854478",
+    "url": "./static/media/arrow_next_min.cbd381fd.svg"
+  },
+  {
+    "revision": "ce7fb65e1042a2dbf02bfb5ddb381bb7",
+    "url": "./static/media/arrow_prev.ce7fb65e.svg"
+  },
+  {
+    "revision": "de7ec34aa03a6c75c0b2d2a760661609",
+    "url": "./static/media/cantina_main.de7ec34a.png"
+  },
+  {
+    "revision": "40b67f1d19312b61df506a5fa428b2ec",
+    "url": "./static/media/circle-logo.40b67f1d.svg"
+  },
+  {
+    "revision": "72a2f3df455fd876c202f61675dd82a6",
+    "url": "./static/media/cross.72a2f3df.svg"
+  },
+  {
+    "revision": "cb1d2968f075d07599161a9516b3b717",
+    "url": "./static/media/facebook.cb1d2968.svg"
+  },
+  {
+    "revision": "716b8a5b44ad11bfde01f7b788149a01",
+    "url": "./static/media/fst_scrn_1.716b8a5b.jpg"
+  },
+  {
+    "revision": "b3b6d9221a369f97af38c88888968949",
+    "url": "./static/media/fst_scrn_2.b3b6d922.jpg"
+  },
+  {
+    "revision": "bdf8b6bbe3b6864633f458cd532faf35",
+    "url": "./static/media/fst_scrn_3.bdf8b6bb.jpg"
+  },
+  {
+    "revision": "89c2e66a27f691965e2206f4d382bc52",
+    "url": "./static/media/gif_1.89c2e66a.gif"
+  },
+  {
+    "revision": "c20392e7ae6e48ad91c4fa09203a2fa0",
+    "url": "./static/media/instagram.c20392e7.svg"
+  },
+  {
+    "revision": "6c47a4c0a816cd25ab6a16bf16e8dcac",
+    "url": "./static/media/logo.6c47a4c0.svg"
+  },
+  {
+    "revision": "0d3817df9d5cae256eddba13bd74a91c",
+    "url": "./static/media/logo_font.0d3817df.svg"
+  },
+  {
+    "revision": "a9a183b131f792bacb96542afcc0e35b",
+    "url": "./static/media/logo_vela.a9a183b1.svg"
+  },
+  {
+    "revision": "6b192b41c5181e12c90c82a59b23d8aa",
+    "url": "./static/media/main_back.6b192b41.png"
+  },
+  {
+    "revision": "8a5250c5a2f28fa1a8411cfce27d055f",
+    "url": "./static/media/mapPinter.8a5250c5.svg"
+  },
+  {
+    "revision": "6e4c01dd454ee1e8b6bd192e8dd3bd38",
+    "url": "./static/media/read-more.6e4c01dd.svg"
+  },
+  {
+    "revision": "60abc025d1a0ce0d434e3013bfec4e07",
+    "url": "./static/media/second_block.60abc025.png"
+  },
+  {
+    "revision": "7201fcc4d5e913df3a2655907782e533",
+    "url": "./static/media/tacos_first_block.7201fcc4.png"
+  },
+  {
+    "revision": "a784d264816cc447affc918e87f3da29",
+    "url": "./static/media/tacos_main.a784d264.png"
+  },
+  {
+    "revision": "8f9f37b5ca3231e05c80de3562d04ae4",
+    "url": "./static/media/telegram.8f9f37b5.svg"
+  },
+  {
+    "revision": "e0cc8aedac7f44d116f7dc88266a8b3f",
+    "url": "./static/media/third_block.e0cc8aed.png"
+  },
+  {
+    "revision": "b78b20b9550f3bbb69460a6b271ac915",
+    "url": "./static/media/triangle.b78b20b9.svg"
+  },
+  {
+    "revision": "cb3e6c290b2fa4e2b55fe0b8ce64fa0b",
+    "url": "./static/media/triangle_big.cb3e6c29.svg"
+  },
+  {
+    "revision": "442333146fee3da38565bb5329006dc0",
+    "url": "./static/media/video-icon.44233314.svg"
+  }
+]);
